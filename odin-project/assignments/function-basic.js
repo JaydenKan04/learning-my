7f3ad330707ae699b43dc7df2cf53c8ac62786bd
JaydenKan <kanmanheen@gmail.com>
2024-03-12 21:55:16 +0800

    Completed function basic assignment

---
+++ odin-project/assignments/function-basic.js
@@ -0,0 +1,25 @@
+//Q1
+function add7(x){
+    return x + 7;
+}
+
+console.log(add7(6));
+
+//Q2
+function multiply(x, y){
+    return x*y;
+}
+
+//Q3
+function capitalize(x){
+    return x.charAt(0).toUpperCase() + x.slice(1);
+}
+
+console.log(capitalize("lmao"));
+
+//Q4
+let lastLetter = (x) => {
+    return x.charAt(x.length - 1);
+}
+
+console.log(lastLetter("hello"))
