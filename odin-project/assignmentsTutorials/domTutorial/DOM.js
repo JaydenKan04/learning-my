9311cbded8991988d941add8d47b66d75fc74c5d
JaydenKan <kanmanheen@gmail.com>
2024-03-17 23:04:02 +0800

    Learnt DOM

---
+++ odin-project/assignmentsTutorials/domTutorial/DOM.js
@@ -0,0 +1,22 @@
+// const body = document.body;
+
+// const div = document.createElement('div');
+// const strong = document.createElement('strong');
+// strong.innerText = 'Hello World 2';
+
+// div.append(strong);
+// body.appendChild(div);
+
+const body = document.body
+const div = document.querySelector('div')
+const spanHi = document.querySelector('#hi')
+const spanBye = document.querySelector('#bye')
+
+// div.removeChild(spanHi)
+
+//Can use method for getting attribute instead of getAttribute
+// console.log(spanHi.title);
+
+// console.log(spanHi.setAttribute('title','ssadad'))
+
+spanHi.style.color = "red"
